77be5b12c541f8b60681a91dfa6b5e94d5bd4964
RudraSalunkhe <rudrasalunkhe61@gmail.com>
2025-04-03 17:46:45 +0530

    initial commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ enemy_start_y_min=50
 enemy_start_y_max=150
 enemy_speed_x=4
 enemy_speed_y=40
-bullet_speed=10
+bullet_speed_y=10
 collision_distance=27
 
 pygame.init()
@@ -27,7 +27,7 @@ playerx=player_start_x
 playery=player_start_y
 playerx_change=0
 
-enemeys=[]
+enemyImg=[]
 enemyx=[]
 enemyy=[]
 enemyx_change=[]
@@ -48,4 +48,62 @@ bullety=player_start_y
 bulletx_change=0
 bullety_change=bullet_speed_y
 bullet_state="ready"
+
+score_value=0
+font=pygame.font.Font('freesansbold.ttf',32)
+textx=0
+texty=0
+
+over_font=pygame.font.Font('freesansbold.ttf',64)
+
+def show_score(x,y):
+    score=font.render('score :'+str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+
+def game_over_text():
+    over_text=over_font.render('GAME OVER',True,(255,255,255))
+    screen.blit(over_text(200,250))
+
+def player(x,y):
+    screen.blit(playerimg(x,y))
+
+def enemy(x,y,i):
+    screen.blit(enemyImg[i],(x,y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state="free"
+    screen.blit(bulletImg,(x+16,y+10))
+
+def iscollision(enemyx,enemyy,bulletx,bullety):
+    distance=math.sqrt((enemyx-enemyy)**2,(enemyy-bullety)**2)
+    return distance<collision_distance
+
+running=True
+while  running:
+    screen.fill(0,0,0)
+    screen.blit(background,(0,0))
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT:
+                playery_change=-5
+            if event.key==pygame.K_RIGHT:
+                playerx_change=5
+            if event.key==pygame.K_SPACE and bullet_state=="ready":
+                bulletx=playerx
+            fire_bullet(bulletx,bullety)
+        if event.type==pygame.KEYUP and event.key in (pygame.K_LEFT,pygame.K_RIGHT):
+            playerx_change=0
+
+            playerx=
+
+
+
+
+
+
+
 
